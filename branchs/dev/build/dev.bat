393fc369459211f4d03a4b19023b74b588aaa1db
valiantwu <changwuWoo@outlook.com>
2018-11-15 23:15:56 +0800

    springmvc集成

--- branchs/dev/build/dev.bat
+++ branchs/dev/build/dev.bat
@@ -1,10 +1,12 @@
 if not exist C:\evn-dev md C:\evn-dev
-if not exist C:\evn-dev\trd md c:\evn-dev\trd
-if not exist C:\evn-dev\corelib md c:\evn-dev\corelib
+if not exist C:\evn-dev\mservice\lib\trd md c:\evn-dev\mservice\lib\trd
+if not exist C:\evn-dev\mservice\lib\core md c:\evn-dev\mservice\lib\core
+if not exist C:\evn-dev\web\lib\trd md c:\evn-dev\web\lib\trd
+if not exist C:\evn-dev\web\lib\core md c:\evn-dev\web\lib\core
 
 
-#del /Q C:\evn-dev\trd\*
-#del /Q C:\evn-dev\corelib\*
+#del /Q C:\evn-dev\mservice\*
+#del /Q C:\evn-dev\web\*
 
 
 xcopy /Y /E dev-env\* C:\evn-dev\
