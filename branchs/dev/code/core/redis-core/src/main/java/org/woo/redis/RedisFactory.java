d827cbf030fe189a4f950564841d3308877c4dae
changwuWoo <Shiftedge0608>
2018-11-27 20:48:06 +0800

    redistribute

--- branchs/dev/code/core/redis-core/src/main/java/org/woo/redis/RedisFactory.java
+++ branchs/dev/code/core/redis-core/src/main/java/org/woo/redis/RedisFactory.java
@@ -1,7 +1,9 @@
 package org.woo.redis;
 
+import org.woo.redis.builder.JedisBuilder;
 import redis.clients.util.Pool;
 
+import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 /**
@@ -10,5 +12,38 @@ import java.util.concurrent.ConcurrentHashMap;
  * org.woo.redis.RedisFactory
  */
 public class RedisFactory {
-    private static ConcurrentHashMap<String,Pool> poolMap=new ConcurrentHashMap<>();
+    private static ConcurrentHashMap<String, Pool<?>> poolMap = new ConcurrentHashMap<>();
+
+    private static Pool<?> getPool(String url, boolean rebuild) {
+        Pool<?> pool = null;
+        if (rebuild) {
+            synchronized (RedisFactory.class) {
+                poolMap.remove(url);
+            }
+        } else {
+            pool = poolMap.get(url);
+        }
+        if (pool == null) {
+            synchronized (RedisFactory.class) {
+                pool = poolMap.get(url);
+                if (pool == null) {
+                    try {
+                        pool = new JedisBuilder().build(url);
+                        poolMap.put(url, pool);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+        return pool;
+    }
+
+    private static Map<String, String> redisUrlMap = new ConcurrentHashMap<>();
+
+    public static JedisClient getJedisClient(String url, boolean original) {
+        url = redisUrlMap.get(url);
+        JedisClient jedisClient = (JedisClient) getPool(url, false).getResource();
+        return jedisClient;
+    }
 }
