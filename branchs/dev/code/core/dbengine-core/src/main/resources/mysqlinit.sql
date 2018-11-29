d3c1a71034e22242062b6ac180e9dc7304477bbb
valiantwu <eedgeWoo0608>
2018-11-29 09:42:23 +0800

    整合oracle和mysql

--- branchs/dev/code/core/dbengine-core/src/main/resources/mysqlinit.sql
+++ branchs/dev/code/core/dbengine-core/src/main/resources/mysqlinit.sql
@@ -1,4 +1,4 @@
 CREATE USER 'devUser'@'%' IDENTIFIED BY 'devPassWd';
 DROP USER 'devUser'@'%';
-GRANT SELECT,INSERT ON dev.* TO 'devUser'@'%';
+GRANT SELECT,INSERT,DELETE ON dev.* TO 'devUser'@'%';
 jdbc:mysql://localhost:3306/test
